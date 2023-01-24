85a0a825584860050064b550c0fd1223416fabfc
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2023-01-24 21:41:05 +0000

    feat: reqwest timeout & user agent

--- src/ws/connect.rs
+++ src/ws/connect.rs
@@ -9,26 +9,39 @@ struct PostRequest<'a> {
     password: &'a str,
 }
 
+impl<'a> From<&'a AppEnv> for PostRequest<'a> {
+    fn from(app_envs: &'a AppEnv) -> Self {
+        Self {
+            key: &app_envs.ws_apikey,
+            password: &app_envs.ws_password,
+        }
+    }
+}
 #[derive(Debug, Serialize, Deserialize)]
+// This is an ULID, but probably no need to parse it as such
 struct PostResponse {
-    // This is an UUID, but probably no need to parse it as such
     response: String,
 }
 
 /// Make a http request to get an access token
 async fn get_auth_token(app_envs: &AppEnv) -> Result<String, AppError> {
-    let request_body = PostRequest {
-        key: &app_envs.ws_apikey,
-        password: &app_envs.ws_password,
-    };
-    let request: PostResponse = reqwest::Client::new()
-        .post(&app_envs.ws_token_address)
-        .json(&request_body)
-        .send()
-        .await?
-        .json()
-        .await?;
-    Ok(request.response)
+	let request = reqwest::Client::builder()
+	.connect_timeout(std::time::Duration::from_millis(5000))
+	.gzip(true)
+	.brotli(true)
+	.user_agent(format!(
+		"{}/{}",
+		env!("CARGO_PKG_NAME"),
+		env!("CARGO_PKG_VERSION")
+	))
+	.build()?
+	.post(&app_envs.ws_token_address)
+	.json(&PostRequest::from(app_envs))
+	.send()
+	.await?
+	.json::<PostResponse>()
+	.await?;
+Ok(request.response)
 }
 
 /// Connect to wesbsocket server
