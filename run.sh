77d01dda470892322d7ebb3e99d93df9a5fcf307
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2023-12-30 23:57:37 +0000

    feat: run.sh v0.1.0

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# v0.0.5
+# v0.1.0
 
 DOCKER_GUID=$(id -g)
 DOCKER_UID=$(id -u)
@@ -12,7 +12,7 @@ YELLOW='\033[0;33m'
 RESET='\033[0m'
 
 error_close() {
-	echo -e "\n${RED}ERROR - EXITED: ${YELLOW}$1${RESET}\n";
+	echo -e "\n${RED}ERROR - EXITED: ${YELLOW}$1${RESET}\n"
 	exit 1
 }
 
@@ -20,29 +20,53 @@ if ! [ -x "$(command -v dialog)" ]; then
 	error_close "dialog is not installed"
 fi
 
-
-production_up () {
+production_up() {
 	DOCKER_GUID=${DOCKER_GUID} \
-	DOCKER_UID=${DOCKER_UID} \
-	DOCKER_TIME_CONT=${DOCKER_TIME_CONT} \
-	DOCKER_TIME_CITY=${DOCKER_TIME_CITY} \
-	docker compose up -d
+		DOCKER_UID=${DOCKER_UID} \
+		DOCKER_TIME_CONT=${DOCKER_TIME_CONT} \
+		DOCKER_TIME_CITY=${DOCKER_TIME_CITY} \
+		docker compose up -d
 }
 
-production_down () {
+production_down() {
 	DOCKER_GUID=${DOCKER_GUID} \
-	DOCKER_UID=${DOCKER_UID} \
-	DOCKER_TIME_CONT=${DOCKER_TIME_CONT} \
-	DOCKER_TIME_CITY=${DOCKER_TIME_CITY} \
-	docker compose -f docker-compose.yml down
+		DOCKER_UID=${DOCKER_UID} \
+		DOCKER_TIME_CONT=${DOCKER_TIME_CONT} \
+		DOCKER_TIME_CITY=${DOCKER_TIME_CITY} \
+		docker compose -f docker-compose.yml down
 }
 
-production_rebuild () {
+production_rebuild() {
 	DOCKER_GUID=${DOCKER_GUID} \
-	DOCKER_UID=${DOCKER_UID} \
-	DOCKER_TIME_CONT=${DOCKER_TIME_CONT} \
-	DOCKER_TIME_CITY=${DOCKER_TIME_CITY} \
-	docker compose up -d --build
+		DOCKER_UID=${DOCKER_UID} \
+		DOCKER_TIME_CONT=${DOCKER_TIME_CONT} \
+		DOCKER_TIME_CITY=${DOCKER_TIME_CITY} \
+		docker compose up -d --build
+}
+
+git_pull_branch() {
+	git checkout -- .
+	git checkout main
+	git pull origin main
+	git fetch --tags
+	latest_tag=$(git tag | sort -V | tail -n 1)
+	git checkout -b "$latest_tag"
+}
+
+pull_branch() {
+	GIT_CLEAN=$(git status --porcelain)
+	if [ -n "$GIT_CLEAN" ]; then
+		echo -e "\n${RED}GIT NOT CLEAN${RESET}\n"
+		printf "%s\n" "${GIT_CLEAN}"
+	fi
+	if [[ -n "$GIT_CLEAN" ]]; then
+		ask_yn "Happy to clear git state"
+		if [[ "$(user_input)" =~ ^n$ ]]; then
+			exit
+		fi
+	fi
+	git_pull_branch
+	main
 }
 
 main() {
@@ -51,6 +75,7 @@ main() {
 		1 "${PRO} up" off
 		2 "${PRO} down" off
 		3 "${PRO} rebuild" off
+		4 "pull & branch" off
 	)
 	choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 	exitStatus=$?
@@ -58,20 +83,26 @@ main() {
 	if [ $exitStatus -ne 0 ]; then
 		exit
 	fi
-	for choice in $choices
-	do
+	for choice in $choices; do
 		case $choice in
-			0)
-				exit;;
-			1)
-				production_up
-				break;;
-			2)
-				production_down
-				break;;
-			3)
-				production_rebuild
-				break;;
+		0)
+			exit
+			;;
+		1)
+			production_up
+			break
+			;;
+		2)
+			production_down
+			break
+			;;
+		3)
+			production_rebuild
+			break
+			;;
+		4)
+			pull_branch
+			;;
 		esac
 	done
 }
